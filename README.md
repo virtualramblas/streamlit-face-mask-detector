30ca087c1f12031072ae5d53b2bad0c6afa0372b
virtualramblas <guglielmo.iozzia@gmail.com>
2020-05-17 23:01:56 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # streamlit-face-mask-detector
-A simple Streamlit frontend for a pre-trained MobileNet CNN model + OpenCV for face mask detection in images.
+A simple [Streamlit](https://www.streamlit.io/) frontend for face mask detection in images using a pre-trained [Keras](https://keras.io/) CNN model + [OpenCV](https://opencv.org/) and model interpretability.  
+## General info
+This example has been implemented as part of my evaluation of the Streamlit framework. It uses OpenCV to detect faces in the input images and a CNN as mask/no-mask binary classifier applied to the face ROI. The Deep Learning model currently used has the architecture suggested by Adrian Rosebrock [here](https://www.pyimagesearch.com/2020/05/04/covid-19-face-mask-detector-with-opencv-keras-tensorflow-and-deep-learning/) and has been trained using [this](https://github.com/prajnasb/observations/tree/master/experiements/data) image data set. The trained model has been shared in this repo. The face detector algorithm can be downloaded from [here](https://github.com/Shiva486/facial_recognition): the Caffe model and its descriptor need then to be saved into the *face_detector* directory.    
+## Deep Learning Explanation
+Once an image has been uploaded, the classification happens automatically. It is then possible to apply some interpretability methods for neural network understanding. The UI presents two buttons to apply the following methods:
+- Grad CAM: it visualizes how parts of the input image affect a CNN output by looking into the activation maps.
+- Occlusion Sensitivity: it visualizes how parts of the input image affect a CNN confidence by iteratively occluding parts.
+## Usage
+After cloning this repository and have downloaded the face detector algorithm, you need to create a virtual environment and install the application dependencies  
+```pip install -r requirements.txt```
+Then you can execute the application through the streamlit command  
+```streamlit run streamlit_face_mask_detector.py```
+
---
+++ streamlit_face_mask_detector.py
@@ -0,0 +1,85 @@
+import streamlit as st
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+import cv2
+import os
+
+@st.cache(hash_funcs={cv2.dnn_Net: hash})
+def load_face_detector_and_model():
+    prototxtPath = os.path.sep.join(["face_detector", "deploy.prototxt"])
+    weightsPath = os.path.sep.join(["face_detector",
+                                    "res10_300x300_ssd_iter_140000.caffemodel"])
+    net = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+    return net
+
+@st.cache(allow_output_mutation=True)
+def load_cnn_model():
+    model = load_model("mask_detector.model")
+
+    return model
+
+st.write('# Face Mask Image Detector')
+
+net = load_face_detector_and_model()
+model = load_cnn_model()
+
+uploaded_image = st.file_uploader("Choose a JPG file", type="jpg")
+confidence_value = st.slider('Confidence:', 0.0, 1.0, 0.5, 0.1)
+if uploaded_image:
+    st.image(uploaded_image, width=240)
+    image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    orig = image.copy()
+    (h, w) = image.shape[:2]
+    # construct a blob from the image
+    blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
+                                 (104.0, 177.0, 123.0))
+    # pass the blob through the network and obtain the face detections
+    print("[INFO] computing face detections...")
+    net.setInput(blob)
+    detections = net.forward()
+
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the detection
+        confidence = detections[0, 0, i, 2]
+        # filter out weak detections by ensuring the confidence is
+        # greater than the minimum confidence
+        if confidence > confidence_value:
+            # compute the (x, y)-coordinates of the bounding box for
+            # the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            # ensure the bounding boxes fall within the dimensions of
+            # the frame
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            # extract the face ROI, convert it from BGR to RGB channel
+            # ordering, resize it to 224x224, and preprocess it
+            face = image[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+            face = np.expand_dims(face, axis=0)
+            # pass the face through the model to determine if the face
+            # has a mask or not
+            (mask, withoutMask) = model.predict(face)[0]
+
+            # determine the class label and color we'll use to draw
+            # the bounding box and text
+            label = "Mask" if mask > withoutMask else "No Mask"
+            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+            # include the probability in the label
+            label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+            # display the label and bounding box rectangle on the output
+            # frame
+            cv2.putText(image, label, (startX, startY - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+            cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
+            st.image(image, width=640)
+            st.write('### ' + label)
